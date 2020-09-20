256398c5f4a3d27b8889eeb9692a3e3eb7ec3f94
Loydkim <kimloydmail@gmail.com>
2020-09-20 12:21:22 -0700

    Update const.dart

--- lib/commons/const.dart
+++ lib/commons/const.dart
@@ -3,7 +3,7 @@ final List<String> iconImageList = ['001-panda.png','002-lion.png','003-tiger.pn
   '021-goose.png','022-lemur.png','023-ostrich.png','024-owl.png','025-boar.png','026-penguin.png','027-camel.png','028-raccoon.png','029-hippo.png','030-monkey.png',
   '031-meerkat.png','032-snake.png','033-zebra.png','034-donkey.png','035-bull.png','036-goat-1.png','037-goat.png','038-horse.png','039-wolf.png','040-koala.png',
   '041-hedgehog.png','042-frog.png','043-turtle.png','044-gorilla.png','045-giraffe.png','046-deer.png','047-rhinoceros.png','048-elephant.png','049-puma.png','050-fox.png'];
-const String firebaseCloudserverToken ='AAAAt7GY6Lo:APA91bHEq2Xi6m4716ciM8KgnGcHYw7YJ8K5X3_pFnzbzRkZzyX4nKRkbQFn8pKceSWVFXoYjYuToqGZcnrEhHqhI9gRG7OxQHtrjypm8o2LElq5v0zhOw0Sb64itx54DtpDfb9Du86H';
+const String firebaseCloudserverToken ='YOUR_FIREBASE_SERVER';//'AAAAt7GY6';
 class MyProfileData{
   final String myThumbnail;
   final String myName;
